a4f64aa4cc8b4418c3a0eda8466048627e0dce9c
mahmoudibrahim <mahmoud.guc@gmail.com>
2020-01-12 10:25:47 +0100

    starting to add code and private assets

---
+++ .customlib.r
@@ -0,0 +1,71 @@
+makeTransparent = function(..., alpha=0.5) {
+	alpha = floor(255*alpha)  
+	newColor = col2rgb(col=unlist(list(...)), alpha=FALSE)
+	.makeTransparent = function(col, alpha) {
+		rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+	}
+	newColor = apply(newColor, 2, .makeTransparent, alpha=alpha)
+	return(newColor)
+}
+color.gradient = function(x, colors=c("red","yellow","green"), colsteps=100) {
+	colgrad = colorRampPalette(colors, bias = 10) (colsteps) [ findInterval(x, seq(min(x),max(x), length.out=colsteps)) ]
+	return(colgrad)
+}
+legend.col = function(col, lev){
+	opar = par
+	n = length(col)
+	bx = par("usr")
+	box.cx = c(bx[2] + (bx[2] - bx[1]) / 1000,
+	bx[2] + (bx[2] - bx[1]) / 1000 + (bx[2] - bx[1]) / 50)
+	box.cy = c(bx[3], bx[3])
+	box.sy = (bx[4] - bx[3]) / n
+	xx = rep(box.cx, each = 2)
+	par(xpd = TRUE)
+	for(i in 1:n){
+		yy = c(box.cy[1] + (box.sy * (i - 1)),
+		box.cy[1] + (box.sy * (i)),
+		box.cy[1] + (box.sy * (i)),
+		box.cy[1] + (box.sy * (i - 1)))
+		polygon(xx, yy, col = col[i], border = col[i])
+	}
+	par(new = TRUE)
+	plot(0, 0, type = "n",
+	ylim = c(min(lev), max(lev)),
+	yaxt = "n", ylab = "",
+	xaxt = "n", xlab = "",
+	frame.plot = FALSE)
+	axis(side = 4, las = 2, tick = FALSE, line = .25)
+	par	= opar
+}
+minor.ticks.axis = function(ax,n,t.ratio=0.5,mn,mx,...){
+	lims = par("usr")
+	if(ax %in%c(1,3)) lims = lims[1:2] else lims[3:4]
+
+	major.ticks = pretty(lims,n=5)
+	if(missing(mn)) mn = min(major.ticks)
+	if(missing(mx)) mx = max(major.ticks)
+	
+	major.ticks = major.ticks[major.ticks >= mn & major.ticks <= mx]
+	labels = sapply(major.ticks,function(i) as.expression(bquote(10^ .(i))))
+	axis(ax,at=major.ticks,labels=labels,...)
+
+	n = n+2
+	minors = log10(pretty(10^major.ticks[1:2],n))-major.ticks[1]
+	minors = minors[-c(1,n)]
+	
+	minor.ticks = c(outer(minors,major.ticks,`+`))
+	minor.ticks = minor.ticks[minor.ticks > mn & minor.ticks < mx]
+	
+	axis(ax,at=minor.ticks,tcl=par("tcl")*t.ratio,labels=FALSE)
+}
+score = function(x){
+	x  = ((x-min(x))/(max(x)-min(x)))
+	return(x)
+}
+dist2d = function(a,b,c) {
+	v1 = b - c
+	v2 = a - b
+	m = cbind(v1,v2)
+	d = abs(det(m))/sqrt(sum(v1*v1))
+	return(d)
+}
