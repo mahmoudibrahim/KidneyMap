74883e7e6cfba53062c2ecdae78b9f7a374c1f8c
mahmoudibrahim <mahmoud.guc@gmail.com>
2020-01-12 11:18:06 +0100

    add license info

--- make_intergrated_maps/mouse_PDGFRBpositive.r
+++ make_intergrated_maps/mouse_PDGFRBpositive.r
@@ -1,3 +1,23 @@
+########################################################################
+# mouse_PDGFRBpositive.r
+#
+# Copyright (C) 2019-2020  Mahmoud M Ibrahim
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# Contact: mmibrahim@pm.me (Mahmoud M Ibrahim)
+########################################################################
 rm(list = ls())
 options(scipen=1000000)
 
