c4a924bed2d6bb880dc9e5b7759f0ccfb58e17c8
Mahmoud Ibrahim <mahmoud.guc@gmail.com>
2020-01-14 15:51:21 +0100

    started adding templates

---
+++ templates/makeUMAP.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import umap
+import random
+
+random.seed(111)
+
+##unsupervised full
+csv = np.genfromtxt("dims.txt", delimiter=",") #read in SVD left singular vectors
+fit = umap.UMAP(n_neighbors=50, min_dist = 0.6, random_state=111) #general whole integrated map umaps //  number of neighbors: 150 for CD10+/- data // 50 for other data
+#fit = umap.UMAP(n_neighbors = 80, min_dist = 0, random_state=111) #for "lineage" UMAP - num. neighbors: 46 for CD10- mesenchymal data, 89 for PDGFRb mesenchymal data
+u = fit.fit_transform(csv)
+np.savetxt("xx_umapCoords.csv", u, delimiter = ",") #save the UMAP coords files
+
+
